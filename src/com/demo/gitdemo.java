e7eba43895802159d651a3348e786e3e45bf2c5b
Gaurav Seth <gauravvcorporate@gmail.com>
2021-06-06 23:55:23 +0530

    second heroku automatic ci check

--- src/com/demo/gitdemo.java
+++ src/com/demo/gitdemo.java
@@ -3,5 +3,6 @@ package com.demo;
 public class gitdemo {
 public static void main(String[] args) {
 	System.out.println("Simple heroku Deployment check");
+	System.out.println("Second automatic CI check on heroku");
 }
 }
